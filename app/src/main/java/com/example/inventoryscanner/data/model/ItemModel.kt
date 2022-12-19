f6db2bbefc1e5e6b856fb8f8b192a92ab36c3bc9
Choirul Rahmaditya <choirul.rahmaditya@dattabot.io>
2022-12-19 13:14:23 +0100

    [Choi] Insert almost done, only connecting to db left

---
+++ app/src/main/java/com/example/inventoryscanner/data/model/ItemModel.kt
@@ -0,0 +1,8 @@
+package com.example.inventoryscanner.data.model
+
+data class ItemModel(
+    var id: String? = null,
+    var name: String? = null,
+    var room: String? = null,
+    var information: String? = null
+)
